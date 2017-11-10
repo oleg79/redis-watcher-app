7ce8cc410d9b605e93b3b53621e2a9cb8408bcf9
oleg79 <0112oleg@gmail.com>
2017-11-10 04:10:57 +0300

    FIXED: karma.config

--- karma.config.js
+++ karma.config.js
@@ -1,54 +1,48 @@
 const webpackConfig = require('./webpack.config');
 
-const configuration = {
-  // Paths
-  basePath: '',
-  exclude: [],
-  files: [
-    { pattern: 'src/**/*.test.ts', watch: false },
-  ],
-
-  // Module processing
-  preprocessors: {
-    // Process all *test* modules with webpack
-    // (it will handle dependencies)
-    'src/**/*.test.ts': ['webpack', 'sourcemap'],
-  },
-
-  // Webpack config
-  webpack: webpackConfig,
-  webpackMiddleware: {
-    stats: 'errors-only',
-  },
-
-  browsers: ['PhantomJS'],
-
-  customLaunchers: {
-    Chrome_travis_ci: {
-      base: 'Chrome',
-      flags: ['--no-sandbox']
-    }
-  },
-
-  // Reporters
-  reporters: ['dots'],
-  logLevel: config.LOG_INFO,
-  colors: true,
-
-  // Test framework configuration
-  frameworks: ['jasmine'],
-
-  // Runner configuration
-  port: 9876,
-  autoWatch: true,
-  singleRun: true,
-  concurrency: Infinity
-};
-
-if (process.env.TRAVIS) {
-  configuration.browsers = ['Chrome_travis_ci'];
-}
-
 module.exports = (config) => {
-  config.set(configuration);
+  config.set({
+    // Paths
+    basePath: '',
+    exclude: [],
+    files: [
+      { pattern: 'src/**/*.test.ts', watch: false },
+    ],
+
+    // Module processing
+    preprocessors: {
+      // Process all *test* modules with webpack
+      // (it will handle dependencies)
+      'src/**/*.test.ts': ['webpack', 'sourcemap'],
+    },
+
+    // Webpack config
+    webpack: webpackConfig,
+    webpackMiddleware: {
+      stats: 'errors-only',
+    },
+
+    browsers: process.env.TRAVIS ? ['Chrome_travis_ci'] : ['PhantomJS'],
+
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+
+    // Reporters
+    reporters: ['dots'],
+    logLevel: config.LOG_INFO,
+    colors: true,
+
+    // Test framework configuration
+    frameworks: ['jasmine'],
+
+    // Runner configuration
+    port: 9876,
+    autoWatch: true,
+    singleRun: true,
+    concurrency: Infinity
+  });
 };
