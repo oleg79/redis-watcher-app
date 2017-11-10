f20e6bd019eaf5ddddbf649f63a9dcd4a58ed919
oleg79 <0112oleg@gmail.com>
2017-11-10 04:43:52 +0300

    FIXED: karma travis fixes

--- karma.config.js
+++ karma.config.js
@@ -22,14 +22,14 @@ module.exports = (config) => {
       stats: 'errors-only',
     },
 
-    browsers: process.env.TRAVIS ? ['Chrome'] : ['PhantomJS'],
+    browsers: process.env.TRAVIS ? ['Chrome_travis_ci'] : ['PhantomJS'],
 
-    // customLaunchers: {
-    //   Chrome_travis_ci: {
-    //     base: 'Chrome',
-    //     flags: ['--no-sandbox']
-    //   }
-    // },
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
 
     // Reporters
     reporters: ['dots'],
