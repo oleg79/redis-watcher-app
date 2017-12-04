d78807a976aaecec800516a5f2e04ceb7d2046da
oleg79 <0112oleg@gmail.com>
2017-12-04 23:08:44 +0300

    ADDED: keys list filtration

--- src/components/info-list/KeysList.vue
+++ src/components/info-list/KeysList.vue
@@ -1,21 +1,36 @@
 <template>
-  <b-list-group flush v-if="keysSet && keysSet.length">
-    <b-list-group-item
-      v-for="setKey in keysSet"
-      key="key"
-      @click="getKeyValue(setKey)"
-      :class="{ active: setKey === currentKey }"
-    >
-      {{ setKey }}
-    </b-list-group-item>
-  </b-list-group>
+  <div class="keys-list-container" v-if="keysSet && keysSet.length">
+    <div class="input-group keys-list-filter">
+      <span class="input-group-addon"><icon name="search" scale="1"></icon></span>
+      <input type="text" class="form-control" placeholder="" v-model="keyName">
+    </div>
+    <b-list-group flush>
+      <b-list-group-item
+        v-for="setKey in filteredKeys"
+        key="key"
+        @click="getKeyValue(setKey)"
+        :class="{ active: setKey === currentKey }"
+      >
+        {{ setKey }}
+      </b-list-group-item>
+    </b-list-group>
+  </div>
 </template>
 
 <script>
   import { mapState, mapMutations } from 'vuex'
   export default {
+    data() {
+      return {
+        keyName: ''
+      }
+    },
+
     computed: {
-      ...mapState(['keysSet', 'currentKey'])
+      ...mapState(['keysSet', 'currentKey']),
+      filteredKeys () {
+        return this.keysSet.filter(key => key.includes(this.keyName))
+      }
     },
 
     methods: {
